f0495ffde244a616dc50cf877b9a6a45fc7f2daf
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2019-11-26 21:59:00 +0100

    Version nueva

--- baraja.py
+++ baraja.py
@@ -1,5 +1,31 @@
 import random
 
+_palos = ['o', 'c', 'e', 'b']
+_cartas = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
+
+def baraja():
+    result = []
+    for palo in _palos:
+        for carta in _cartas:
+            result.append(carta + palo)
+
+    return result
+
+def elige_carta(i, longitud):
+    return random.randint(0, longitud-1)
+
+def mezclar(b):
+    for i in range(len(b)):
+        #Elegimos carta de intercambio al azar
+        al_azar = elige_carta(i, len(b))
+
+        #Cambiamos la carta por orden (i) por la carta elegida al azar
+        aux = b[i]
+        b[i] = b[al_azar]
+        b[al_azar] = aux
+
+    return b
+
 class Baraja():
     naipes = []
     __palos = ["o", "c", "e", "b"]
@@ -11,17 +37,25 @@ class Baraja():
             for carta  in self.__cartas:
                 self.naipes.append(carta + palo)
 
-    def elige_carta(self, i, longitud):
+    def elige_carta(self, i):
         return random.randint(0, len(self.naipes)-1)
 
     def mezclar(self):
         for i in range(len(self.naipes)):
-            al_azar = self.elige_carta, len(self.naipes)
+            al_azar = self.elige_carta(i)
 
             aux = self.naipes[i]  
             self.naipes[i] = self.naipes[al_azar]
             self.naipes[al_azar] = aux
 
     def repartir(self, mano, jugadores):
-        pass
-    
+        def repartir(self, mano, jugadores):
+            jugadas = []
+        for i in range(jugadores):
+            jugadas.append([])
+            for j in range(mano):
+                jugadas[i].append(self.naipes[j*jugadores+i])
+                
+        cartas = mano * jugadores
+        self.naipes = self.naipes[cartas:]
+        return jugadas
