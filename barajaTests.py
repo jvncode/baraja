c5ba9169db69ea38b677ed95df459c80b97bbdb3
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2019-11-26 20:30:33 +0100

    Fase 2

--- barajaTests.py
+++ barajaTests.py
@@ -4,29 +4,52 @@ import baraja
 
 def mi_eligecarta(i, longitud):
     if i == longitud-1:
-     return 0
+        return 0
     else:
         return i+1
+def class_eligecarta(self, i, longitud):
+    return mi_eligecarta(i, longitud)
 
-class BarajaTest(unittest.TestCase):
+
+class BarajaFuncionalTest(unittest.TestCase):
 
     def test_crear_baraja(self):
-        b = baraja.baraja()
-        self.assertEqual(len(b), 40)
+        b = baraja.Baraja()
+        self.assertEqual(len(b.naipes), 40)
         self.assertEqual(b, ['Ao', '2o', '3o', '4o', '5o', '6o', '7o', 'So', 'Co', 'Ro', 'Ac', '2c', '3c', '4c', '5c', '6c', '7c', 'Sc', 'Cc', 'Rc', 'Ae', '2e', '3e', '4e', '5e', '6e', '7e', 'Se', 'Ce', 'Re', 'Ab', '2b', '3b', '4b', '5b', '6b', '7b', 'Sb', 'Cb', 'Rb'])
 
-        self.assertEqual(b[0], "Ao")
-        self.assertEqual(b[39], "Rb")
-        self.assertEqual(b[10], "Ac")
-        self.assertEqual(b[20], "Ae")
+        self.assertEqual(b.naipes[0], "Ao")
+        self.assertEqual(b.naipes[39], "Rb")
+        self.assertEqual(b.naipes[10], "Ac")
+        self.assertEqual(b.naipes[20], "Ae")
 
-    @patch("baraja.elige_carta", mi_eligecarta)
+    @patch("baraja.Baraja.elige_carta", mi_eligecarta)
     def test_mezclar_lista(self):
         b = ["A", "B", "C", "D", "E"]
         mezclada = ["A", "C", "D", "E", "B"]
-        b = baraja.mezclar(b)
+        b = baraja.Baraja.mezclar(b)
         self.assertEqual(b, mezclada)
 
+class BarajaObjetoTest(unittest.TestCase):
+    def test_crear_baraja(self):
+        b = baraja.Baraja()
+        self.assertEqual(len(b.naipes), 40)
+        self.assertEqual(b.naipes, ['Ao', '2o', '3o', '4o', '5o', '6o', '7o', 'So', 'Co', 'Ro', 'Ac', '2c', '3c', '4c', '5c', '6c', '7c', 'Sc', 'Cc', 'Rc', 'Ae', '2e', '3e', '4e', '5e', '6e', '7e', 'Se', 'Ce', 'Re', 'Ab', '2b', '3b', '4b', '5b', '6b', '7b', 'Sb', 'Cb', 'Rb'])
+
+        self.assertEqual(b.naipes[0], "Ao")
+        self.assertEqual(b.naipes[39], "Rb")
+        self.assertEqual(b.naipes[10], "Ac")
+        self.assertEqual(b.naipes[20], "Ae")
+
+    @patch("baraja.Baraja.elige_carta", class_eligecarta)
+    def test_mezclar_lista(self):
+        b = baraja.Baraja()
+        b.mezclar()
+
+        self.assertEqual(b.naipes[0], "Ao")
+        self.assertEqual(b.naipes[39], "2o")
+        self.assertEqual(b.naipes[10], "2c")
+        self.assertEqual(b.naipes[20], "2e")
 
 
 if __name__ == "__main__":
