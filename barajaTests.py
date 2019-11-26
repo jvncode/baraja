2c9ecdafeed68261510cfd33a81e94b30e2c0ca1
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2019-11-26 21:59:57 +0100

    Nueva versión Tests

--- barajaTests.py
+++ barajaTests.py
@@ -7,9 +7,8 @@ def mi_eligecarta(i, longitud):
         return 0
     else:
         return i+1
-def class_eligecarta(self, i, longitud):
-    return mi_eligecarta(i, longitud)
-
+def class_eligecarta(self, i):
+    return mi_eligecarta(i, len(self.naipes))
 
 class BarajaFuncionalTest(unittest.TestCase):
 
@@ -25,9 +24,8 @@ class BarajaFuncionalTest(unittest.TestCase):
 
     @patch("baraja.Baraja.elige_carta", mi_eligecarta)
     def test_mezclar_lista(self):
-        b = ["A", "B", "C", "D", "E"]
-        mezclada = ["A", "C", "D", "E", "B"]
-        b = baraja.Baraja.mezclar(b)
+        b = baraja.Baraja()
+        b.mezclar()
         self.assertEqual(b, mezclada)
 
 class BarajaObjetoTest(unittest.TestCase):
@@ -51,6 +49,14 @@ class BarajaObjetoTest(unittest.TestCase):
         self.assertEqual(b.naipes[10], "2c")
         self.assertEqual(b.naipes[20], "2e")
 
+    def test_repartir_baraja(self):
+        b = baraja.Baraja()
+        jugadas = b.repartir(5,3)
+        self.assertEqual(len(jugadas),3)
+        self.assertEqual(len(b.naipes),25)
+        self.assertEqual(jugadas[0], ['Ao', '4o', '7o','Ro','3c'])
+        self.assertEqual(jugadas[1], ['2o', '5o', 'So','Ac','4c'])
+        self.assertEqual(jugadas[2], ['3o', '6o', 'Co','2c','5c'])
 
 if __name__ == "__main__":
     unittest.main()
